7bef4d673a2d9f9aed1320977b56f3b54a5f5c11
hsjoo126 <hsjoo126@gmail.com>
2025-01-11 14:43:35 +0900

    고중배당 페이지 임시 완성 250111 1. views.py - def middle 작성완료 1-1. 많은 데이터를 불러올 수 없기에, 100개까지만 불러오도록 설정함 2. middle.html 임시 작성완료 2-1. view에서 보낸 데이터들이 잘 뜨는지 확인하기 위해 테이블 정도만 임시로 만듦 문제점 : 데이터를 불러올 때 너무 많은 시간이 소요돼서 다 안 불러와짐, 과부하 줄일 방법을 찾아야함

--- dividend_stock/stocks/views.py
+++ dividend_stock/stocks/views.py
@@ -1,14 +1,68 @@
 from django.shortcuts import render
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+import FinanceDataReader as fdr
 
 
 def main(request):
     return render(request, "stocks/main.html")
 
+
 def high(request):
     return render(request, "stocks/high.html")
 
+
 def middle(request):
-    return render(request, "stocks/middle.html")
+    # 나스닥(NASDAQ) 주식 정보 가져오기
+    nasdaq_stocks = fdr.StockListing('NASDAQ')
+
+    # Symbol 열만 추출
+    symbols = nasdaq_stocks['Symbol']
+
+    tickers = symbols[:100]
+
+    stock_data = []
+    for ticker_symbol in tickers:
+        ticker = yf.Ticker(ticker_symbol)
+
+        # 현재 주가
+        current_price = ticker.info.get('currentPrice', '정보 없음')
+
+        # 마지막 배당금
+        dividends = ticker.dividends
+        last_dividend_value = dividends.iloc[-1] if not dividends.empty else None
+
+        # 마지막 배당일
+        info = ticker.calendar
+        dividend_date = None
+
+        # info가 dict 형식인 경우, Dividend Date 항목을 확인
+        if isinstance(info, dict) and 'Dividend Date' in info:
+            dividend_date = info['Dividend Date']
+
+        # info가 DataFrame 형식인 경우, Dividends 항목이 존재하는지 확인
+        elif isinstance(info, pd.DataFrame) and 'Dividends' in info.index:
+            dividend_date = info.loc['Dividends', 0]
+
+        # 배당수익률
+        yield_value = ticker.info.get('dividendYield', 0) * 100
+
+        # 시가총액
+        market_cap = ticker.info.get('marketCap', '정보 없음')
+
+        if 4 <= yield_value <= 7:
+            stock_data.append({
+                'ticker': ticker_symbol,
+                'current_price': current_price,
+                'last_dividend': last_dividend_value,
+                'dividend_date': dividend_date,
+                'dividend_yield': round(yield_value, 2),
+                'market_cap': "{:,}".format(market_cap) if isinstance(market_cap, int) else market_cap,
+            })
+
+    return render(request, "stocks/middle.html", {"stocks_data": stock_data})
+
 
 def detail(request):
     return render(request, "stocks/detail.html")
