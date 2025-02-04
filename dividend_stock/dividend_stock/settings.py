dac765efc8d598be38dd147222e51c5e8d09c790
hsjoo126 <hsjoo126@gmail.com>
2025-02-04 12:06:04 +0900

    장고 시크릿키 재설정, env 관련 설정 250204 1. env 파일 생성 후 시크릿키 넣고 env는 .gitignore 에 추가 2. settings.py에 env 참조할 수 있도록 설정함.(secret key도 참조할 수 있도록)

--- dividend_stock/dividend_stock/settings.py
+++ dividend_stock/dividend_stock/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# 환경변수 초기화
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'REMOVED_SECRET_KEY'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
